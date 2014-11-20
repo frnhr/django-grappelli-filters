57c4e8af1188f675af34407bad6e13484e82058b
Piotr Jakimiak <piotr.jakimiak@gmail.com>
2014-11-20 09:14:54 +0100

    Fixed autocomplete filter

--- grappelli_filters/filters.py
+++ grappelli_filters/filters.py
@@ -62,7 +62,7 @@ class RelatedAutocompleteFilter(AbstractFieldListFilter):
             content_type = ContentType.objects.get_for_model(self.model)
         else:
             content_type = ContentType.objects.get_for_model(getattr(model, self.field_path).field.rel.to)
-        self.grappelli_trick = u'/{app_label}/{model_name}/t=id'.format(
+        self.grappelli_trick = u'/{app_label}/{model_name}/'.format(
             app_label=content_type.app_label,
             model_name=content_type.model
         )
